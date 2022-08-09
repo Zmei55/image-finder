2a1a81341fc3ee129a2dc3a98efaa743f7c7ea11
Andrei <97633022+Zmei55@users.noreply.github.com>
2022-08-09 21:12:28 +0200

    Loader

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,6 +4,10 @@ import { Component } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem';
 import { LoadButton } from 'components/Button';
 import PicturesApiService from '../../services/pictures-api';
+import { Loader } from 'components/Loader';
+import { Modal } from 'components/Modal';
+// import { ThreeDots } from 'react-loader-spinner';
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { GalleryContainer } from './ImageGallery.styled';
 
 const picturesApiService = new PicturesApiService();
@@ -11,29 +15,46 @@ const picturesApiService = new PicturesApiService();
 export class ImageGallery extends Component {
   state = {
     pictures: null,
+    loading: false,
+    error: null,
+    showModal: true,
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchQuery !== this.props.searchQuery) {
+      this.setState({ loading: true });
+
       picturesApiService.query = this.props.searchQuery;
       picturesApiService.resetPage();
       picturesApiService
         .fetchPictures()
-        .then(pictures => this.setState({ pictures: pictures.hits }));
+        .then(pictures => this.setState({ pictures: pictures.hits }))
+        .catch(error => this.setState({ error }))
+        .finally(() => this.setState({ loading: false }));
     }
   }
 
   onLoadMore = () => {
+    this.setState({ loading: true });
+
     picturesApiService.incrementPage();
-    picturesApiService.fetchPictures().then(pictures =>
-      this.setState(prevState => ({
-        pictures: [...prevState.pictures, ...pictures.hits],
-      })),
-    );
+    picturesApiService
+      .fetchPictures()
+      .then(pictures =>
+        this.setState(prevState => ({
+          pictures: [...prevState.pictures, ...pictures.hits],
+        })),
+      )
+      .catch(error => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
 
   render() {
-    const { pictures } = this.state;
+    const { pictures, loading, showModal } = this.state;
 
     return (
       <React.Fragment>
@@ -49,7 +70,13 @@ export class ImageGallery extends Component {
             ))}
           </GalleryContainer>
         )}
-        {pictures && <LoadButton onClick={this.onLoadMore} />}
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src="" alt="" />
+          </Modal>
+        )}
+        {loading && <Loader />}
+        {pictures && !loading && <LoadButton onClick={this.onLoadMore} />}
       </React.Fragment>
     );
   }
