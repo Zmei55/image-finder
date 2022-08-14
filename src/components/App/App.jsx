d06c8f49c19643bee14a96e1ad149558f6cb6e5d
Andrei <97633022+Zmei55@users.noreply.github.com>
2022-08-14 23:05:57 +0200

    App hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Container } from './App.styled';
 import { Searchbar } from 'components/Searchbar';
 import { ImageGallery } from 'components/ImageGallery';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
+export function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleFormSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
   };
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery });
-  };
-
-  render() {
-    return (
-      <Container>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchQuery={this.state.searchQuery} />
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Searchbar onSubmit={handleFormSubmit} />
+      <ImageGallery searchQuery={searchQuery} />
+    </Container>
+  );
 }
 
 App.propTypes = {
   searchQuery: PropTypes.string,
 };
+
+//? class
+
+// import React, { Component } from 'react';
+
+// export class App extends Component {
+//   state = {
+//     searchQuery: '',
+//   };
+
+//   handleFormSubmit = searchQuery => {
+//     this.setState({ searchQuery });
+//   };
+
+//   render() {
+//     return (
+//       <Container>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         <ImageGallery searchQuery={this.state.searchQuery} />
+//       </Container>
+//     );
+//   }
+// }
